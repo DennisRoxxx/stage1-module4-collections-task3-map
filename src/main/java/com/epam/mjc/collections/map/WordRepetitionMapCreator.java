1e1a62e70d593915df7166bf3af115c616f1ec2d
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-29 10:42:14 +0300

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -4,5 +4,22 @@ import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        if (sentence.isEmpty()) {
+            return map;
+        }
+        
+        String[] text = sentence.split(". , ! ? : ; + - = _ [ ] ( )");
+        
+        for (String temp : text) {
+            String word = temp.toLowerCase();
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
     }
 }
