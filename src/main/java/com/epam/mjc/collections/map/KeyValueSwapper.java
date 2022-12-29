bbc1bed87b342ee5ee74cbee5fc4f5da6c80f871
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-29 10:48:28 +0300

    Update KeyValueSwapper.java

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,14 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String,Integer> swapMap = new HashMap<>();
+        for (Map.Entry<Integer, String> sw : sourceMap.entrySet()) {
+            swapMap.put(sw.getValue(), sw.getKey());
+        }
+        return swapMap;
     }
 }
